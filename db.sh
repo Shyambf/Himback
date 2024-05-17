33fd501ed6bcf2f1d0b791c725fd531124091dfa
Shyam <samborisov@gmail.com>
2024-05-17 18:52:25 +0300

    del trash

--- db.sh
+++
@@ -1,8 +0,0 @@
-psql -U postgres postgres < /dumps/init.sql
-#!/bin/bash
-
-read -p "Do you want to add test data to the table? (y/n): " choice
-
-if [ "$choice" = "y" ]; then
-    psql -U postgres postgres < /dumps/add_data.sql
-fi
