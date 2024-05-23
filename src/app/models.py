3535f1248d3a86d9090615aef2b3625d286eb0a0
Shyam <samborisov@gmail.com>
2024-05-23 15:05:23 +0300

    fix model`s names

--- src/app/models.py
+++ src/app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Service(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    duration_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField()
 
     def __str__(self):
         return self.name
@@ -28,7 +28,7 @@ class Client(models.Model):
     ]
 
     id = models.AutoField(primary_key=True)
-    full_name = models.CharField(max_length=255)
+    fio = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
     email = models.EmailField()
@@ -39,11 +39,11 @@ class Client(models.Model):
 
 class Visit(models.Model):
     id = models.AutoField(primary_key=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    service_id = models.ForeignKey(Service, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
-    service_rendered = models.BooleanField(default=False)
+    service_fullfilled = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.client.full_name} - {self.service.name}'
