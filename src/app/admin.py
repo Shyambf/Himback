aa2c7a18fdb62021076a46fbe33c4f4fa9ed8357
Shyam <samborisov@gmail.com>
2024-05-23 15:08:57 +0300

    delete admin panel

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,18 +1,1 @@
-from django.contrib import admin
-from .models import Service, Client, Visit
 
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'cost', 'duration_minutes']
-    list_filter = ['cost']
-
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ['id', 'full_name', 'phone_number', 'category', 'email']
-    list_filter = ['category']
-
-class VisitAdmin(admin.ModelAdmin):
-    list_display = ['id', 'client', 'service', 'date', 'time', 'service_rendered']
-    list_filter = ['date', 'service']
-
-admin.site.register(Service, ServiceAdmin)
-admin.site.register(Client, ClientAdmin)
-admin.site.register(Visit, VisitAdmin)
