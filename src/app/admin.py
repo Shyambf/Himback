c70e6684d272446c9014cc2470345764088a3a96
Shyam <samborisov@gmail.com>
2024-05-23 14:30:25 +0300

    removed unnecessary classes, serializers, admin panels

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,51 +1,18 @@
 from django.contrib import admin
-from .models import (
-    ServiceGroup, Position, Service, Client, Employee, Visit, Cosmetic, CosmeticPurchase
-)
+from .models import Service, Client, Visit
 
-@admin.register(ServiceGroup)
-class ServiceGroupAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
-
-@admin.register(Position)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'service_group', 'work_schedule')
-    search_fields = ('name',)
-    list_filter = ('service_group',)
-
-@admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'group', 'cost', 'duration')
-    search_fields = ('name',)
-    list_filter = ('group',)
+    list_display = ['id', 'name', 'cost', 'duration_minutes']
+    list_filter = ['cost']
 
-@admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'full_name', 'phone_number', 'category', 'email')
-    search_fields = ('full_name', 'phone_number', 'email')
-    list_filter = ('category',)
+    list_display = ['id', 'full_name', 'phone_number', 'category', 'email']
+    list_filter = ['category']
 
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'full_name', 'position', 'address', 'phone_number')
-    search_fields = ('full_name', 'phone_number', 'address')
-    list_filter = ('position',)
-
-@admin.register(Visit)
 class VisitAdmin(admin.ModelAdmin):
-    list_display = ('id', 'client', 'service', 'employee', 'date', 'time', 'service_rendered')
-    search_fields = ('client__full_name', 'service__name', 'employee__full_name')
-    list_filter = ('date', 'service_rendered')
+    list_display = ['id', 'client', 'service', 'date', 'time', 'service_rendered']
+    list_filter = ['date', 'service']
 
-@admin.register(Cosmetic)
-class CosmeticAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'cost', 'type')
-    search_fields = ('name',)
-    list_filter = ('type',)
-
-@admin.register(CosmeticPurchase)
-class CosmeticPurchaseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'date', 'client', 'position', 'quantity', 'cost_per_item', 'total_cost')
-    search_fields = ('client__full_name', 'position__name')
-    list_filter = ('date',)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Visit, VisitAdmin)
